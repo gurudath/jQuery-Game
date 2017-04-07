79cc0a151d186946b49c28af10ee516dbcd98160
gurudath bn <gurudath.bn@siriusemb.com>
2017-04-07 15:54:11 +0530

    making the background

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@
           	var options = $.extend({ x: default_values.x, y: default_values.y , width: default_values.width, height: default_values.height }, options);
           	ggame.gameFramework.addSprite(default_values.id,'sprite1',options)
           	$(obj).css("position", "relative").append("<div id='sprite1' style='position: absolute'>");
-		      	$("#"+default_values.subchild_id).css({"backgroundImage":"url("+"./images/sprint1.png"+")","min-height":default_values.height,"max-height":default_values.height,"min-width":default_values.width,"max-width":default_values.width});
+		      	$("#"+default_values.subchild_id).css({"backgroundImage":"url("+default_values.url+")","min-height":default_values.height,"max-height":default_values.height,"min-width":default_values.width,"max-width":default_values.width});
             ggame.gameFramework.addSprite(default_values.child_id, default_values.subchild_id, default_values)
         }     
 
@@ -48,16 +48,10 @@
     };
 })(jQuery);
 
-/* 
-   Sample Usage Of the Plugin*/
+
 $(document).ready(function () {
    var backgroundAnim = new ggame.gameImage.animation({
-     //url : "./images/sprint1.png",
-     //url : "http://orig13.deviantart.net/cacc/f/2017/040/4/6/spritesheet_zector_sem_armadura_by_tiozacdasgalaxias-daygx0l.png",
-     //url : "https://commons.wikimedia.org/wiki/File:00_592a_Zweirichtungswagen_547.jpg",
-     //url : "https://upload.wikimedia.org/wikipedia/commons/a/a0/%27Greeley_Panorama%27_from_Opportunity%27s_Fifth_Martian_Winter%2C_PIA15689.jpg",
-     url : 'https://upload.wikimedia.org/wikipedia/commons/f/fe/01R_Oct_12_2012_0905Z.jpg',
-     //url : "https://upload.wikimedia.org/wikipedia/commons/2/2d/Snake_River_%285mb%29.jpg",
+     url : "http://orig13.deviantart.net/cacc/f/2017/040/4/6/spritesheet_zector_sem_armadura_by_tiozacdasgalaxias-daygx0l.png",
      numberOfFrame:8
    });
    function initialize(){
@@ -68,7 +62,13 @@ $(document).ready(function () {
    $("#startButton").click(function() {
       ggame.gameImage.startPreloading(initialize,ggame.gameImage.processCall);
    });
-
-	
 });
 
+
+
+
+//url : "./images/sprint1.png",
+//url : "https://commons.wikimedia.org/wiki/File:00_592a_Zweirichtungswagen_547.jpg",
+//url : "https://upload.wikimedia.org/wikipedia/commons/a/a0/%27Greeley_Panorama%27_from_Opportunity%27s_Fifth_Martian_Winter%2C_PIA15689.jpg",
+//url : 'https://upload.wikimedia.org/wikipedia/commons/f/fe/01R_Oct_12_2012_0905Z.jpg',
+//url : "https://upload.wikimedia.org/wikipedia/commons/2/2d/Snake_River_%285mb%29.jpg",
